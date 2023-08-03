5a33048bb2b12b0dcc7a6604b0510a8b67a989e4
minliacom <minliacom@gmail.com>
2023-08-03 18:35:04 +0800

    add getBaseUrl of api.tsx

--- apps/chrome/src/utils/api.tsx
+++ apps/chrome/src/utils/api.tsx
@@ -7,24 +7,20 @@ import superjson from "superjson";
 import type { AppRouter } from "@acme/api";
 
 export const api = createTRPCReact<AppRouter>();
-const getBaseUrl = () => {
-  /**
-   * Gets the IP address of your host-machine. If it cannot automatically find it,
-   * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
-   * you don't have anything else running on it, or you'd have to change it.
-   *
-   * **NOTE**: This is only for development. In production, you'll want to set the
-   * baseUrl to your production API URL.
-   */
-  // const localhost = Constants.manifest?.debuggerHost?.split(":")[0];
-  // if (!localhost) {
-  //   // return "https://your-production-url.com";
-  //   throw new Error(
-  //     "Failed to get localhost. Please point to your production server.",
-  //   );
-  // }
-  return `http://localhost:8001`;
-};
+function getBaseUrl() {
+  if (typeof window !== "undefined")
+    // browser should use relative path
+    return "";
+  if (process.env.VERCEL_URL)
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`;
+  if (process.env.RENDER_INTERNAL_HOSTNAME)
+    // reference for render.com
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 8002}`;
+}
+
 export const TRPCProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
---
+++ apps/nextjs/src/app/api/test/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function POST(req: NextRequest) {
+  console.log("POST hi");
+  //   return NextResponse.json({ message: "hi" }, { status: 200 });
+  const result = { message: "hi" };
+
+  return new Response(JSON.stringify(result), {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
+
+//curl -i -X 'POST'  -H 'Content-Type: text/plain;charset=UTF-8'  --data-raw '{"owner":"Significant-Gravitas","repo":"Auto-GPT"}' "http://localhost:5175/api/hi"
